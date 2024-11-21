37d3c51b464c185e61b7da5e72c0d1d3cd68ebb2
Aaron Saju Augustine <asajadidas@gmail.com>
2024-11-21 15:31:41 -0700

    Initial upload of source code upto right before anti-aliasing

---
+++ renderer/CMakeFiles/renderer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/renderer.dir/renderer.cpp.o"
+  "renderer.pdb"
+  "renderer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
